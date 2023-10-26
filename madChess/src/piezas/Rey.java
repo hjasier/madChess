6edb20f4592818760eeb403df9a1b13bb7ad6c79
gipas <giovanni.pacchetti@opendeusto.es>
2023-10-26 11:37:09 +0200

    Rey movimientos

--- madChess/src/piezas/Rey.java
+++ madChess/src/piezas/Rey.java
@@ -34,19 +34,18 @@ public class Rey extends Pieza implements MetodosInterfaz{
             char nuevaColumna = (char) (columna + movimiento[1]);
 
             // Verifica que la casilla resultante esté dentro del tablero (filas 0 a 7 y columnas A a H)
-            while (nuevaFila >= 0 && nuevaFila <= 7 && nuevaColumna >= 'A' && nuevaColumna <= 'H') {
+           
             	
                 Casilla casillaDisp = casillas.get(nuevaFila * 8 + (nuevaColumna - 'A'));
                 
-                if (casillaDisp.getPieza()!=null&&casillaDisp.getPieza().getIsWhite()==this.getIsWhite()) {break;}
                 casillasDisp.add(casillaDisp);
 
                 // Avanzo a la dirección del movimiento
                 nuevaFila += movimiento[0];
                 nuevaColumna = (char) (nuevaColumna + movimiento[1]);
-                if (casillaDisp.getPieza()!=null) {break;}
+                
             }
-        }
+        
 
         return casillasDisp;
     }
