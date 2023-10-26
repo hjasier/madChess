784b15a3a00d99221e38d2c4fde30bfabeb5e9d5
gotzon.viteri <gotzon.viteri@E105L14.opendeusto.es>
2023-10-26 11:19:41 +0200

    movimiento alfil y fix tamanyo circulos

--- madChess/src/piezas/Alfil.java
+++ madChess/src/piezas/Alfil.java
@@ -12,5 +12,40 @@ public class Alfil extends Pieza implements MetodosInterfaz {
 		super("b",isWhite);
 	}
 
-	
+	@Override
+    public ArrayList<Casilla> getCasillasDisponibles(Casilla curCasilla, ArrayList<Casilla> casillas) {
+        int casillaIndex = casillas.indexOf(curCasilla);
+        ArrayList<Casilla> casillasDisp = new ArrayList<>();
+        int fila = curCasilla.getFila();
+        char columna = curCasilla.getColumna();
+
+        // Posibles movimientos de la torre: arriba, abajo, izquierda y derecha
+        int[][] movimientos = {
+        			
+        		{-1, -1},    {-1, 1},
+        		      //Alfil//
+        		{1, -1},    {1, 1}
+        };
+
+        for (int[] movimiento : movimientos) { //Recorre cada movimiento posible desde la posicion de la torre
+            int nuevaFila = fila + movimiento[0];
+            char nuevaColumna = (char) (columna + movimiento[1]);
+
+            // Verifica que la casilla resultante esté dentro del tablero (filas 0 a 7 y columnas A a H)
+            while (nuevaFila >= 0 && nuevaFila <= 7 && nuevaColumna >= 'A' && nuevaColumna <= 'H') {
+            	
+                Casilla casillaDisp = casillas.get(nuevaFila * 8 + (nuevaColumna - 'A'));
+                
+                if (casillaDisp.getPieza()!=null&&casillaDisp.getPieza().getIsWhite()==this.getIsWhite()) {break;}
+                casillasDisp.add(casillaDisp);
+
+                // Avanzo a la dirección del movimiento
+                nuevaFila += movimiento[0];
+                nuevaColumna = (char) (nuevaColumna + movimiento[1]);
+                if (casillaDisp.getPieza()!=null) {break;}
+            }
+        }
+
+        return casillasDisp;
+    }
 }
