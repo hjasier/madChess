105d23dfbfea88fdf58e7c04a0612256548d2174
asier.aurre <asier.aurre@E105L13.opendeusto.es>
2023-10-26 11:24:18 +0200

    a

--- madChess/src/objetos/Casilla.java
+++ madChess/src/objetos/Casilla.java
@@ -3,9 +3,10 @@ package objetos;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Image;
-
+import java.awt.RenderingHints;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
@@ -107,7 +108,10 @@ public class Casilla extends JPanel {
 
 	@Override
     protected void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g; // para que se vea nice
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         super.paintComponent(g);
+        
         
         g.setColor(this.color);
         int height = Math.min(getWidth(), getHeight());
@@ -118,7 +122,7 @@ public class Casilla extends JPanel {
         int x = (getWidth() - imgSize) / 2;
         int y = (getHeight() - imgSize) / 2;
         
-
+        
         if (this.isDisponible) {
         	g.setColor(new Color(255, 201, 0 ));
         	if (this.pieza != null) {
