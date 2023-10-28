9938d7e2bee5a3f4a41c12ec7252da78d5ca8cc1
Josu <Josu@PC-B-Rabbit>
2023-10-28 19:59:41 +0200

    Panel del chat y los movimientos de piezas + chat hecho

--- madChess/src/ventanas/VentanaJuegoTesteos.java
+++ madChess/src/ventanas/VentanaJuegoTesteos.java
@@ -9,6 +9,8 @@ import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
@@ -18,6 +20,9 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import objetos.*;
 
@@ -26,6 +31,22 @@ public class VentanaJuegoTesteos extends JFrame {
 	private static final long serialVersionUID = 1L;
 	protected JPanel panelJuego;
 	protected JPanel panelControles;
+	
+	protected JPanel panelChat;
+	protected JTextArea areaChat;
+	protected JTextField textfieldChat;
+	protected JTextField textfieldUsuario;
+	protected JLabel labelChat;
+	protected JLabel labelEscribe;
+	protected JButton botonEnviar;
+	protected JPanel panelControlChat;
+	protected JPanel panelDatosChat;
+	protected JPanel panelUsuario;
+	protected JPanel panelMensaje;
+	protected JLabel labelUsuario;
+	
+	
+	protected JPanel panelMovimentos;
 	
 	protected TableroTests tablero = new TableroTests();
 	
@@ -40,7 +61,7 @@ public class VentanaJuegoTesteos extends JFrame {
 
 	    // Paneles
 	    JPanel panelJuego = new JPanel();
-	    JPanel panelControles = new JPanel();
+	    JPanel panelControles = new JPanel(new GridLayout(2,1));
 
 	    // Colores
 	    fondo.setBackground(Color.red);
@@ -77,7 +98,59 @@ public class VentanaJuegoTesteos extends JFrame {
 	    
 	    System.out.println(panelJuego.getSize());
 	    
+	    // panel controles, movimiento y chat
 	    
+	    
+	    panelMovimentos = new JPanel();
+	    
+	    panelChat = new JPanel( new BorderLayout());
+	    panelControlChat = new JPanel(new GridLayout(2,1));
+	    
+	    labelChat = new JLabel("CHAT");
+	    areaChat = new JTextArea(5,2);
+	    textfieldChat = new JTextField(0);
+	    JScrollPane scrollChat = new JScrollPane(areaChat);
+	    labelUsuario = new JLabel("Usuario:");
+	    labelEscribe = new JLabel("Mensaje:");
+	    panelDatosChat = new JPanel( new GridLayout(1,2));
+	    textfieldUsuario = new JTextField(0);
+	    botonEnviar = new JButton("Enviar");
+	    botonEnviar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String texto = textfieldChat.getText();
+				String usuario = textfieldUsuario.getText();
+                areaChat.append(usuario + ": " + texto + "\n"); // Agrega el texto al JTextArea
+                textfieldChat.setText(""); // Limpia el JTextField después de agregar el texto
+                textfieldUsuario.setText("");
+				
+			}
+		});
+	    
+	    panelChat.add(labelChat, BorderLayout.NORTH);
+	    panelChat.add(scrollChat, BorderLayout.CENTER);
+	    
+	    panelUsuario = new JPanel(new BorderLayout());
+	    panelMensaje = new JPanel(new BorderLayout());
+	    
+	    panelUsuario.add(labelUsuario, BorderLayout.WEST);
+	    panelUsuario.add(textfieldUsuario, BorderLayout.CENTER);
+	    panelMensaje.add(labelEscribe, BorderLayout.WEST);
+	    panelMensaje.add(textfieldChat , BorderLayout.CENTER);
+	    
+	    panelDatosChat.add(panelUsuario);
+	    panelDatosChat.add(panelMensaje);
+	    
+	    
+	    
+	    panelControlChat.add(panelDatosChat);
+	    
+	    panelControlChat.add(botonEnviar);
+	    
+	    panelChat.add(panelControlChat, BorderLayout.SOUTH); 
+	    panelControles.add(panelMovimentos);
+	    panelControles.add(panelChat);
         
         
 
@@ -88,7 +161,7 @@ public class VentanaJuegoTesteos extends JFrame {
         
 
         tablero.recalcularTamanyo();
-        
+      
         //PARA VER EL TABLEOR DE ANTES CAMBIAR EL ARCHIVO MAIN.JAVA	
         
     
