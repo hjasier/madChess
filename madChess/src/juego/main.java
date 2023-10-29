041eb850742c378a1e8b2e0e4deace77eb542f9e
Josu <Josu@PC-B-Rabbit>
2023-10-29 12:16:24 +0100

    ventana login (login y signup)

--- madChess/src/juego/main.java
+++ madChess/src/juego/main.java
@@ -1,13 +1,14 @@
 package juego;
 
 import ventanas.VentanaJuegoTesteos;
+import ventanas.VentanaLogin;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Partida v1 = new Partida();
-		
+		new VentanaLogin();
 		new VentanaJuegoTesteos();
 	}
 
