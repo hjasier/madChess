e540dea5e51337938bbf254ad18cc24deb863175
Potzon <dekumalditonerd@gmail.com>
2023-11-18 23:07:45 +0100

    temporizador

--- madChess/src/componentes/userInfo.java
+++ madChess/src/componentes/userInfo.java
@@ -5,18 +5,23 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class userInfo extends JPanel{
 	
 	protected JLabel labelUsuario; 
 	protected JLabel labelTemp; 
-	
+	protected int tiempoRestante;
 	
 	public userInfo() {
+		tiempoRestante = 100;
+		
 		this.setLayout(new BorderLayout());
 		this.setMaximumSize(new Dimension(Integer.MAX_VALUE, 35));
 		this.setBackground(new Color(16,16,16));
@@ -37,6 +42,23 @@ public class userInfo extends JPanel{
 		
 		this.add(labelUsuario,BorderLayout.WEST);
 		this.add(labelTemp,BorderLayout.EAST);
-	}
-	
+		
+		
+		 Timer timer = new Timer(1000, new ActionListener() { //Temporizador
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                // Cambia el tiempo en labelTemp
+	                tiempoRestante--;
+	                labelTemp.setText(String.valueOf(tiempoRestante));
+
+	                // Si el tiempo llega a 0, el temporizador para
+	                if (tiempoRestante == 0) {
+	                    ((Timer) e.getSource()).stop();
+	                }
+	            }
+	        });
+
+	       
+	        timer.start();
+	    }
 }
