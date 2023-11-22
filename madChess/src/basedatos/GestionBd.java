b1ee1b0200b5e0f08a0ff7159de9095aa4627813
josuviteri <josu.viteri@opendeusto.es>
2023-11-22 10:38:58 +0100

    gestion partidas en la bd

--- madChess/src/basedatos/GestionBd.java
+++ madChess/src/basedatos/GestionBd.java
@@ -15,7 +15,7 @@ public class GestionBd {
    
 	public static void crearTablaUsuario() {
 	        String sql = "CREATE TABLE IF NOT EXISTS Usuario (\n"
-	                + "    username TEXT PRIMARY KEY NOT NULL,\n"
+	                + "    username TEXT NOT NULL,\n"
 	                + "    passw TEXT NOT NULL,\n"
 	                + "    img_route TEXT NOT NULL,\n"
 	                + "    rank INTEGER NOT NULL, \n"
@@ -32,6 +32,24 @@ public class GestionBd {
 	            e.printStackTrace();
 	        }
 	    }
+	
+	public static void crearTablaPartida() {
+        String sql = "CREATE TABLE IF NOT EXISTS Partida (\n"
+                + "    gameId TEXT NOT NULL,\n"
+                + "    FechaIni TEXT NOT NULL,\n"
+                + "    FechaFin TEXT NOT NULL,\n"
+                + "    rank INTEGER NOT NULL, \n"
+                + "    PRIMARY KEY(gameId)"	
+                + ");";
+        
+        
+        try (Connection conn = ConexionBd.obtenerConexion();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 	 public static void insertarUsuario(String username, String passw, String img_route, int rank) {
 		    // Verificar si el usuario ya existe antes de insertar
@@ -55,6 +73,27 @@ public class GestionBd {
 		    }
 		}
 	 
+	 public static void insertarPartida(String gameId, String fechaIni, String FechaFin) {
+		    // Verificar si el usuario ya existe antes de insertar
+		    if (!existeUsuario(gameId)) {
+		        String sql = "INSERT INTO Partida(gameId, fechaIni, FechaFin ) VALUES(?,?,?)";
+
+		        try (Connection conn = ConexionBd.obtenerConexion();
+		             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+		            pstmt.setString(1, gameId);
+		            pstmt.setString(2, fechaIni);
+		            pstmt.setString(3, FechaFin);
+		            
+		            pstmt.executeUpdate();
+		            System.out.println("Partida insertada correctamente.");
+		        } catch (SQLException e) {
+		            e.printStackTrace();
+		        }
+		    } else {
+		        System.out.println("La partida ya existe. No se puede insertar.");
+		    }
+		}
+	 
 	 
 	
 	 public static void eliminarUsuarios(String username) {
@@ -74,10 +113,27 @@ public class GestionBd {
 		 }
 	 }
 	 
+	 public static void eliminarPartida(String gameId) {
+		 if(existeUsuario(gameId)) {
+			 String sql = "DELETE FROM Partida WHERE gameId = ? ";
+			 
+			 try (Connection conn = ConexionBd.obtenerConexion();
+		            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+		            
+		            pstmt.executeUpdate();
+		            System.out.println("Partida eliminada correctamente.");
+		        } catch (Exception e) {
+				e.printStackTrace();
+			}
+		 } else {
+			 System.out.println("No se puede eliminar una partida inexistente");
+		 }
+	 }
+	 
 	 
 	 public static void modificarUsuarios(String username, String passw, String img_route, int rank) {
 		 if(existeUsuario(username)) {
-			 String sql = "UPDATE Usuario SET username = ? , passw = ? WHERE username = ?";
+			 String sql = "UPDATE Usuario SET username = ? , passw = ?, img_route = ?, rank = ? WHERE username = ?";
 			 
 			 try (Connection conn = ConexionBd.obtenerConexion();
 		            PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -93,6 +149,26 @@ public class GestionBd {
 				}
 		 } else {
 			 System.out.println("No se puede modificar un usuario inexistente");
+		 }
+	 }
+	 
+	 public static void modificarPartida(String gameId, String fechaIni, String FechaFin) {
+		 if(existeUsuario(gameId)) {
+			 String sql = "UPDATE Partida SET gameId = ? , fechaIni = ?, FechaFin = ? WHERE username = ?";
+			 
+			 try (Connection conn = ConexionBd.obtenerConexion();
+		            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+		            pstmt.setString(1, gameId);
+		            pstmt.setString(2, fechaIni);
+		            pstmt.setString(3, FechaFin);
+		            
+		            pstmt.executeUpdate();
+		            System.out.println("Partida modificada correctamente.");
+		        } catch (Exception e) {
+					e.printStackTrace();
+				}
+		 } else {
+			 System.out.println("No se puede modificar una partida inexistente");
 		 }
 	 }
 
@@ -124,6 +200,19 @@ public class GestionBd {
 	        }
 	    }
 
+	    public static void mostrarPartidas() {
+	        String sql = "SELECT * FROM Partida";
+
+	        try (Connection conn = ConexionBd.obtenerConexion();
+	             PreparedStatement pstmt = conn.prepareStatement(sql);
+	             ResultSet rs = pstmt.executeQuery()) {
+	            while (rs.next()) {
+	                System.out.println("gameId: " + rs.getString("gameId") + "\tFechaIni: " + rs.getInt("fechaIni") + "\tFechaFin: " + rs.getInt("FechaFin"));
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
 	   
 		public static boolean existeUsuario(String username) {
 	        String sql = "SELECT * FROM Usuario WHERE username = ?";
@@ -131,6 +220,20 @@ public class GestionBd {
 	        try (Connection conn = ConexionBd.obtenerConexion();
 	             PreparedStatement pstmt = conn.prepareStatement(sql)) {
 	            pstmt.setString(1, username);
+	            ResultSet rs = pstmt.executeQuery();
+	            return rs.next();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	            return false;
+	        }
+	    }
+		
+		public static boolean existePartida(String gameId) {
+	        String sql = "SELECT * FROM Partida WHERE gameId = ?";
+
+	        try (Connection conn = ConexionBd.obtenerConexion();
+	             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+	            pstmt.setString(1, gameId);
 	            ResultSet rs = pstmt.executeQuery();
 	            return rs.next();
 	        } catch (SQLException e) {
