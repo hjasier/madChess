c5bf9bfdba8d9d9fa885df80367f46ca1b1a6c4e
GiovanniPacchetti <giovanni.pacchetti@opendeusto.es>
2023-11-23 21:58:40 +0100

    PanelBoost

--- madChess/src/componentes/PanelBoost.java
+++ madChess/src/componentes/PanelBoost.java
@@ -1,133 +1,86 @@
 package componentes;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
 
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import java.awt.Component;
-import javax.swing.SwingConstants;
+import javax.swing.*;
+
+import juego.Configuracion;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PanelBoost extends JPanel {
-    protected JPanel panelBoosts = new JPanel();
-    protected JPanel panelDesc = new JPanel();
-
-    protected JPanel panelBoost1 = new JPanel();
-    protected JPanel panelBoost1View = new JPanel();
-    protected JLabel labelBoost1Nombre;
-    protected JLabel labelBoost1Icono;
-    protected MButton botonBoost1;
-    protected JLabel labelBoost1Cargas;
-
-    protected JPanel panelBoost2 = new JPanel();
-    protected JPanel panelBoost2View = new JPanel();
-    protected JLabel labelBoost2Nombre;
-    protected JLabel labelBoost2Icono;
-    protected MButton botonBoost2;
-    protected JLabel labelBoost2Cargas;
-
-    
-    protected JPanel panelBoost3 = new JPanel();
-    protected JPanel panelBoost3View = new JPanel();
-    protected JLabel labelBoost3Nombre;
-    protected JLabel labelBoost3Icono;
-    protected MButton botonBoost3;
-    protected JLabel labelBoost3Cargas;
-    
-    protected JLabel iconoDesc;
-    protected JLabel labelDesc;
+    Color colorFondo = Configuracion.BACKGROUND;
 
     public PanelBoost() {
-        this.setLayout(new BorderLayout());
-        panelBoosts.setLayout(new BoxLayout(panelBoosts, BoxLayout.Y_AXIS));
-        panelDesc.setLayout(new BorderLayout());
-        panelBoost1View.setLayout(new BorderLayout());
-        
-        
-        panelDesc.setBackground(Color.GREEN);
-        panelBoosts.setBackground(Color.RED);
+        setLayout(new BorderLayout());
+        setBackground(colorFondo);
 
-        panelBoost1.setBackground(Color.ORANGE);
-        panelBoost1View.setBackground(Color.BLUE);
+        // Panel de elementos
+        JPanel panelElementos = new JPanel();
+        panelElementos.setLayout(new BoxLayout(panelElementos, BoxLayout.Y_AXIS));
+        panelElementos.setBackground(colorFondo);
 
-        panelBoost2.setBackground(Color.pink);
-        panelBoost2View.setBackground(Color.BLUE);
-        
-        
+        // Agrega elementos al panel de elementos (puedes agregar más según tus necesidades)
+        for (int i = 1; i <= 10; i++) {
+            JPanel elementoPanel = crearElementoPanel("Boost" + i, "icono ");
+            panelElementos.add(elementoPanel);
+        }
 
-        labelBoost1Nombre = new JLabel("Bomba");
-        labelBoost1Icono = new JLabel("IconoB");
-        labelBoost1Icono.setHorizontalAlignment(SwingConstants.CENTER);
-        botonBoost1 = new MButton("Usar");
-        labelBoost1Cargas = new JLabel("1/1");
+        MScrollPane scrollPane = new MScrollPane(panelElementos);
+        scrollPane.setBorder(null);
+        add(scrollPane, BorderLayout.CENTER);
 
-        labelBoost2Nombre = new JLabel("Hielo");
-        labelBoost2Icono = new JLabel("IconoH");
-        botonBoost2 = new MButton("Usar");
-        labelBoost2Cargas = new JLabel("2/2");
-        
-        labelBoost3Nombre = new JLabel("Jamaicano");
-        labelBoost3Icono = new JLabel("IconoJ");
-        botonBoost3 = new MButton("Usar");
-        labelBoost3Cargas = new JLabel("2/3");
+        // Panel Descripción
+        JPanel panelDescripcion = new JPanel(new BorderLayout());
+        JLabel iconoLabel = new JLabel("Icono");
+        iconoLabel.setHorizontalAlignment(JLabel.LEFT);
+        panelDescripcion.add(iconoLabel, BorderLayout.WEST);
 
-        iconoDesc = new JLabel("Icono");
-        labelDesc = new JLabel("Descripcion");
-        labelDesc.setHorizontalAlignment(JLabel.CENTER);
-        
+        JLabel descripcionLabel = new JLabel("Descripción");
+        descripcionLabel.setHorizontalAlignment(JLabel.CENTER);
+        panelDescripcion.add(descripcionLabel, BorderLayout.CENTER);
 
-        panelBoost1View.add(labelBoost1Nombre, BorderLayout.WEST);
-        panelBoost1View.add(labelBoost1Icono, BorderLayout.CENTER);
-        
-        botonBoost1.setPreferredSize (new Dimension(200, 60));
-        panelBoost1.setLayout(new BoxLayout(panelBoost1, BoxLayout.X_AXIS));
-        
-        
-        
-        
-        panelBoost1.add(panelBoost1View);
-        panelBoost1.add(Box.createRigidArea(new Dimension(80,0)));
-        panelBoost1.add(botonBoost1);
-        panelBoost1.add(labelBoost1Cargas);
-        //panelBoost1.setMaximumSize(new Dimension(getMaximumSize().width,250));
-        
-        panelBoost2View.add(labelBoost2Nombre, BorderLayout.WEST);
-        panelBoost2View.add(labelBoost2Icono, BorderLayout.CENTER);
+        panelDescripcion.setBackground(colorFondo);
+        add(panelDescripcion, BorderLayout.SOUTH);
+    }
 
-        panelBoost2.add(panelBoost2View, BorderLayout.WEST);
-        panelBoost2.add(botonBoost2, BorderLayout.CENTER);
-        panelBoost2.add(labelBoost2Cargas, BorderLayout.EAST);
-        
-        //panelBoost2.setBorder(BorderFactory.createEmptyBorder(0,0,100,0));
-        
-        panelBoost3View.add(labelBoost3Nombre, BorderLayout.WEST);
-        panelBoost3View.add(labelBoost3Icono, BorderLayout.CENTER);
-        
-        panelBoost3.add(panelBoost3View, BorderLayout.WEST);
-        panelBoost3.add(botonBoost3, BorderLayout.CENTER);
-        panelBoost3.add(labelBoost3Cargas, BorderLayout.EAST);
-        
-        
-        panelBoosts.add(Box.createRigidArea(new Dimension(0, 40)));
-        panelBoosts.add(panelBoost1);
-        panelBoosts.add(Box.createRigidArea(new Dimension(0, 40)));
-        panelBoosts.add(panelBoost2);
-        panelBoosts.add(Box.createRigidArea(new Dimension(0, 40)));
-        panelBoosts.add(panelBoost3);
-        panelBoosts.add(Box.createRigidArea(new Dimension(0, 40)));
-        //panelBoosts.setPreferredSize(new Dimension(getMaximumSize().width,200));
-        
-        
-        panelDesc.add(iconoDesc, BorderLayout.WEST);
-        panelDesc.add(labelDesc, BorderLayout.CENTER);
+    private JPanel crearElementoPanel(String nombreBoost, String nombreIcono) {
+        JPanel elementoPanel = new JPanel();
+        elementoPanel.setBackground(colorFondo);
+        elementoPanel.setLayout(new BorderLayout());
 
-        this.add(panelBoosts, BorderLayout.CENTER);
-        this.add(panelDesc, BorderLayout.SOUTH);
+        // Panel izquierdo con nombre del Boost y nombre del Icono
+        JPanel panelIzquierdo = new JPanel();
+        panelIzquierdo.setLayout(new GridLayout(1, 2));
+        panelIzquierdo.setBackground(colorFondo);
+        JLabel boostLabel = new JLabel(nombreBoost);
+        JLabel iconoLabel = new JLabel(nombreIcono);
+        panelIzquierdo.add(boostLabel);
+        panelIzquierdo.add(iconoLabel);
+
+        elementoPanel.add(panelIzquierdo, BorderLayout.WEST);
+
+        // Panel para el botón con FlowLayout
+        JPanel panelBoton = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        BButton usarButton = new BButton("Usar");
+        panelBoton.setBackground(colorFondo);
+        usarButton.setPreferredSize(new Dimension(60, 30));
+        panelBoton.add(usarButton);
+        elementoPanel.add(panelBoton, BorderLayout.CENTER);
+
+        // Label a la derecha
+        JLabel defaultLabel = new JLabel("1/3");
+        elementoPanel.add(defaultLabel, BorderLayout.EAST);
+
+        // Configurar ActionListener para el botón
+        usarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Lógica cuando se hace clic en el botón "Usar"
+                JOptionPane.showMessageDialog(null, "Botón 'Usar' clicado para " + nombreBoost);
+            }
+        });
+
+        return elementoPanel;
     }
 }
