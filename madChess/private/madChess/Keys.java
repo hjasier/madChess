d97983f4a07732a37c88f7d1cd929f327cfd8ac7
hjasier <asiaurre@gmail.com>
2023-11-27 17:32:03 +0100

    test?

--- madChess/private/madChess/Keys.java
+++ madChess/private/madChess/Keys.java
@@ -3,5 +3,5 @@ package madChess;
 public class Keys {
     public static final String DBURL = "jdbc:mysql://ynu.h.filess.io:3307/madChess_shelfmetal";
     public static final String DBUSER = "madChess_shelfmetal";
-    public static final String DBPASSWORD = "46JU72RQt4Lz!v@e,f5eF\"T0UtI*lI";
+    public static final String DBPASSWORD = "TEST";
 }
