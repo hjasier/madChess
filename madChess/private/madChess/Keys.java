81f92a3c1156e761ba914c286832db6fa4272d8e
GiovanniPacchetti <giovanni.pacchetti@opendeusto.es>
2023-11-27 17:22:50 +0100

    test private

--- madChess/private/madChess/Keys.java
+++ madChess/private/madChess/Keys.java
@@ -3,5 +3,5 @@ package madChess;
 public class Keys {
     public static final String DBURL = "jdbc:mysql://ynu.h.filess.io:3307/madChess_shelfmetal";
     public static final String DBUSER = "madChess_shelfmetal";
-    public static final String DBPASSWORD = "9cb17400b7e86c5df7a1a682069d3a561639c208";
+    public static final String DBPASSWORD = "46JU72RQt4Lz!v@e,f5eF\"T0UtI*lI";
 }
